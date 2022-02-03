61964152de79fb21d94cef8175cfac656cba992a
Ganeshgit123 <liai.ganeshkumar@gmail.com>
2022-02-03 13:57:13 +0530

    initial commit

---
+++ about-us.php
@@ -0,0 +1,380 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="style/images/favicon.png">
+  <title>Pranil</title>
+  <link rel="stylesheet" type="text/css" href="style/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="style/css/plugins.css">
+  <link rel="stylesheet" type="text/css" href="style/type/type.css">
+  <link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+<body>
+  <div class="content-wrapper">
+    <nav class="navbar bg-white navbar-expand-lg">
+    <?php $page = 'about';
+        include 'header.php' ?>
+</nav>
+    <!-- /.navbar -->
+
+<div class="wrapper blue-wrapper">
+      <div class="container inner">
+        <h2 class="section-title larger text-center">Our Services</h2>
+        <h3 class="display-4 text-center">The full service we are offering is specifically <br class="d-none d-lg-block" />designed to meet your business needs.</h3>
+        <div class="space50"></div>
+        <div class="row">
+          <div class="col-md-10 offset-md-1 col-lg-8 offset-lg-2">
+            <figure><img src="#" srcset="style/images/concept/concept26.png 1x, style/images/concept/concept26@2x.png 2x" alt="" /></figure>
+          </div>
+          <!-- /column -->
+        </div>
+        <!-- /.row -->
+        <div class="space50"></div>
+        <div class="row gutter-80 gutter-md-30 text-center">
+          <div class="col-md-4">
+            <span class="icon icon-blob icon-blob-yellow color-yellow mb-30"><i class="jam jam-flower"></i><span class="step bg-yellow">1</span></span>
+            <h5>Graphic Design</h5>
+            <p class="mb-15">Nulla vitae elit libero elit non porta gravida eget metus cras. Aenean eu leo quam.</p>
+            <div class="text-center"><a href="#" class="more">Learn more</a></div>
+          </div>
+          <!--/column -->
+          <div class="col-md-4"><span class="icon icon-blob icon-blob-purple color-purple mb-30"><i class="jam jam-phone"></i><span class="step bg-purple">2</span></span>
+            <h5>App Development</h5>
+            <p class="mb-15">Nulla vitae elit libero elit non porta gravida eget metus cras. Aenean eu leo quam.</p>
+            <div class="text-center"><a href="#" class="more">Learn more</a></div>
+          </div>
+          <!--/column -->
+          <div class="col-md-4"><span class="icon icon-blob icon-blob-teal color-teal mb-30"><i class="jam jam-layout"></i><span class="step bg-teal">3</span></span>
+            <h5>Web Design</h5>
+            <p class="mb-15">Nulla vitae elit libero elit non porta gravida eget metus cras. Aenean eu leo quam.</p>
+            <div class="text-center"><a href="#" class="more">Learn more</a></div>
+          </div>
+          <!--/column -->
+        </div>
+        <!--/.row -->
+      </div>
+      <!-- /.container -->
+    </div>
+    <!-- /.wrapper -->
+    <div class="wrapper light-wrapper">
+      <div class="container inner">
+        <div class="row align-items-center">
+          <div class="col-lg-7">
+            <figure><img src="#" srcset="style/images/concept/concept25.png 1x, style/images/concept/concept25@2x.png 2x" alt="" /></figure>
+          </div>
+          <!--/column -->
+          <div class="space20 d-md-none"></div>
+          <div class="space50 d-none d-md-block d-lg-none"></div>
+          <div class="col-lg-5 pl-60 pl-md-15">
+            <h2 class="section-title larger">How It Works?</h2>
+            <h3 class="display-4">Here are 3 working steps to organize our business projects.</h3>
+            <div class="space30"></div>
+            <div class="d-flex flex-row justify-content-center">
+              <div>
+                <div class="icon icon-bg icon-bg-teal color-teal mr-25"><span class="number">1</span></div>
+              </div>
+              <div>
+                <h4 class="mb-10">Creative Ideas</h4>
+                <p>Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida. Cras mattis consectetur.</p>
+              </div>
+            </div>
+            <div class="space10 d-none d-md-block"></div>
+            <div class="d-flex flex-row justify-content-center">
+              <div>
+                <div class="icon icon-bg icon-bg-teal color-teal mr-25"><span class="number">2</span></div>
+              </div>
+              <div>
+                <h4 class="mb-10">Magic Touch</h4>
+                <p>Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida. Cras mattis consectetur.</p>
+              </div>
+            </div>
+            <div class="space10 d-none d-md-block"></div>
+            <div class="d-flex flex-row justify-content-center">
+              <div>
+                <div class="icon icon-bg icon-bg-teal color-teal mr-25"><span class="number">3</span></div>
+              </div>
+              <div>
+                <h4 class="mb-10">Data Analysis</h4>
+                <p>Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida. Cras mattis consectetur.</p>
+              </div>
+            </div>
+          </div>
+          <!--/.row -->
+        </div>
+        <!--/column -->
+      </div>
+      <!--/.row -->
+    </div>
+    <!-- /.wrapper -->
+    <div class="wrapper gray-wrapper">
+      <div class="container inner">
+        <div class="row align-items-center">
+          <div class="col-lg-7 order-lg-2 text-center">
+            <figure><img src="#" srcset="style/images/concept/concept23.png 1x, style/images/concept/concept23@2x.png 2x" alt="" /></figure>
+          </div>
+          <!--/column -->
+          <div class="space20 d-md-none"></div>
+          <div class="space50 d-none d-md-block d-lg-none"></div>
+          <div class="col-lg-5 pr-60 pr-md-15">
+            <h2 class="section-title larger">Why Choose Us?</h2>
+            <h3 class="display-4">So here are a few reasons why our valued customers choose us.</h3>
+            <div class="space30"></div>
+            <div id="accordion-1" class="accordion-wrapper simple">
+              <div class="card">
+                <div class="card-header" id="accordion-heading-1-1">
+                  <h5>
+                    <button data-toggle="collapse" data-target="#accordion-collapse-1-1" aria-expanded="true" aria-controls="accordion-collapse-1-1">Money Protection</button>
+                  </h5>
+                </div>
+                <!-- /.card-header -->
+                <div id="accordion-collapse-1-1" class="collapse show" aria-labelledby="accordion-heading-1-1" data-parent="#accordion-1">
+                  <div class="card-body">
+                    <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras mattis consectetur purus sit amet fermentum. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cum sociis natoque penatibus et magnis.</p>
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.collapse -->
+              </div>
+              <!-- /.card -->
+              <div class="card">
+                <div class="card-header" id="accordion-heading-1-2">
+                  <h5>
+                    <button class="collapsed" data-toggle="collapse" data-target="#accordion-collapse-1-2" aria-expanded="false" aria-controls="accordion-collapse-1-2">Strategic Development</button>
+                  </h5>
+                </div>
+                <!-- /.card-header -->
+                <div id="accordion-collapse-1-2" class="collapse" aria-labelledby="accordion-heading-1-2" data-parent="#accordion-1">
+                  <div class="card-body">
+                    <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras mattis consectetur purus sit amet fermentum. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cum sociis natoque penatibus et magnis.</p>
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.collapse -->
+              </div>
+              <!-- /.card -->
+              <div class="card">
+                <div class="card-header" id="accordion-heading-1-3">
+                  <h5>
+                    <button class="collapsed" data-toggle="collapse" data-target="#accordion-collapse-1-3" aria-expanded="false" aria-controls="accordion-collapse-1-3">Marketing Management</button>
+                  </h5>
+                </div>
+                <!-- /.card-header -->
+                <div id="accordion-collapse-1-3" class="collapse" aria-labelledby="accordion-heading-1-3" data-parent="#accordion-1">
+                  <div class="card-body">
+                    <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras mattis consectetur purus sit amet fermentum. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cum sociis natoque penatibus et magnis.</p>
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.collapse -->
+              </div>
+              <!-- /.card -->
+            </div>
+            <!-- /.accordion-wrapper -->
+          </div>
+          <!--/column -->
+        </div>
+        <!--/.row -->
+      </div>
+      <!-- /.container -->
+    </div>
+    <!-- /.wrapper -->
+    <div class="wrapper light-wrapper">
+      <div class="container inner">
+        <h2 class="section-title larger text-center">Our Pricing</h2>
+        <h3 class="display-4 text-center">We offer great prices, premium products <br class="d-none d-lg-block" />and quality service for your business.</h3>
+        <div class="space30"></div>
+        <div class="pricing-wrapper">
+          <div class="pricing-switcher-wrapper switcher">
+            <div class="pricing-switchers">
+              <div class="pricing-switcher pricing-switcher-active">Monthly</div>
+              <div class="pricing-switcher">Yearly</div>
+              <div class="switcher-button bg-default"></div>
+            </div>
+          </div>
+          <div class="space60"></div>
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <div class="pricing panel box bg-white shadow">
+                <div class="panel-heading">
+                  <div class="prices color-dark">
+                    <div class="price price-show"><span class="price-currency">$</span><span class="price-value">9</span> <span class="price-duration">month</span></div>
+                    <div class="price price-hide price-hidden"><span class="price-currency">$</span><span class="price-value">99</span> <span class="price-duration">year</span></div>
+                  </div>
+                  <h4 class="panel-title mb-0">Basic Plan</h4>
+                </div>
+                <!--/.panel-heading -->
+                <div class="panel-body">
+                  <table class="table">
+                    <tr>
+                      <td><strong>1</strong> Project </td>
+                    </tr>
+                    <tr>
+                      <td><strong>100K</strong> API Access </td>
+                    </tr>
+                    <tr>
+                      <td><strong>100MB</strong> Storage </td>
+                    </tr>
+                    <tr>
+                      <td> Custom <strong>Cloud</strong> Services </td>
+                    </tr>
+                    <tr>
+                      <td> Weekly <strong>Reports</strong></td>
+                    </tr>
+                    <tr>
+                      <td> 7/24 <strong>Support</strong></td>
+                    </tr>
+                  </table>
+                </div>
+                <!--/.panel-body -->
+                <div class="panel-footer"> <a href="#" class="btn" role="button">Choose Plan</a></div>
+              </div>
+              <!--/.pricing -->
+            </div>
+            <!--/column -->
+            <div class="col-md-4 popular">
+              <div class="pricing panel box bg-white shadow">
+                <div class="panel-heading">
+                  <div class="prices color-dark">
+                    <div class="price price-show"><span class="price-currency">$</span><span class="price-value">19</span> <span class="price-duration">month</span></div>
+                    <div class="price price-hide price-hidden"><span class="price-currency">$</span><span class="price-value">199</span> <span class="price-duration">year</span></div>
+                  </div>
+                  <h4 class="panel-title mb-0">Premium Plan</h4>
+                </div>
+                <!--/.panel-heading -->
+                <div class="panel-body">
+                  <table class="table">
+                    <tr>
+                      <td><strong>5</strong> Projects </td>
+                    </tr>
+                    <tr>
+                      <td><strong>100K</strong> API Access </td>
+                    </tr>
+                    <tr>
+                      <td><strong>200MB</strong> Storage </td>
+                    </tr>
+                    <tr>
+                      <td> Custom <strong>Cloud</strong> Services </td>
+                    </tr>
+                    <tr>
+                      <td> Weekly <strong>Reports</strong></td>
+                    </tr>
+                    <tr>
+                      <td> 7/24 <strong>Support</strong></td>
+                    </tr>
+                  </table>
+                </div>
+                <!--/.panel-body -->
+                <div class="panel-footer"> <a href="#" class="btn" role="button">Choose Plan</a></div>
+              </div>
+              <!--/.pricing -->
+            </div>
+            <!--/column -->
+            <div class="col-md-4">
+              <div class="pricing panel box bg-white shadow">
+                <div class="panel-heading">
+                  <div class="prices color-dark">
+                    <div class="price price-show"><span class="price-currency">$</span><span class="price-value">49</span> <span class="price-duration">month</span></div>
+                    <div class="price price-hide price-hidden"><span class="price-currency">$</span><span class="price-value">499</span> <span class="price-duration">year</span></div>
+                  </div>
+                  <h4 class="panel-title mb-0">Corporate Plan</h4>
+                </div>
+                <!--/.panel-heading -->
+                <div class="panel-body">
+                  <table class="table">
+                    <tr>
+                      <td><strong>20</strong> Projects </td>
+                    </tr>
+                    <tr>
+                      <td><strong>300K</strong> API Access </td>
+                    </tr>
+                    <tr>
+                      <td><strong>500MB</strong> Storage </td>
+                    </tr>
+                    <tr>
+                      <td> Custom <strong>Cloud</strong> Services </td>
+                    </tr>
+                    <tr>
+                      <td> Weekly <strong>Reports</strong></td>
+                    </tr>
+                    <tr>
+                      <td> 7/24 <strong>Support</strong></td>
+                    </tr>
+                  </table>
+                </div>
+                <!--/.panel-body -->
+                <div class="panel-footer"> <a href="#" class="btn" role="button">Choose Plan</a></div>
+              </div>
+              <!--/.pricing -->
+            </div>
+            <!--/column -->
+          </div>
+          <!--/.row -->
+        </div>
+        <!--/.pricing-wrapper -->
+        <div class="space140"></div>
+        <div class="row">
+          <div class="col-md-10 offset-md-1 col-lg-8 offset-lg-2">
+            <div class="basic-slider owl-carousel gap-small text-center" data-margin="30">
+              <div class="item">
+                <blockquote class="icon larger">
+                  <p>"Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis, est non commodo luctus, nisi erat."</p>
+                  <div class="blockquote-details justify-content-center">
+                    <div class="img-blob blob1"><img src="style/images/art/t5.jpg" alt="" /></div>
+                    <div class="info">
+                      <h6 class="mb-0">Cornelia Gibson</h6>
+                      <span class="meta mb-0">Financial Analyst</span>
+                    </div>
+                  </div>
+                </blockquote>
+              </div>
+              <!-- /.item -->
+              <div class="item">
+                <blockquote class="icon larger">
+                  <p>"Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper. Cras justo odio, dapibus."</p>
+                  <div class="blockquote-details justify-content-center">
+                    <div class="img-blob blob1"><img src="style/images/art/t2.jpg" alt="" /></div>
+                    <div class="info">
+                      <h6 class="mb-0">Coriss Ambady</h6>
+                      <span class="meta mb-0">Marketing Specialist</span>
+                    </div>
+                  </div>
+                </blockquote>
+              </div>
+              <!-- /.item -->
+              <div class="item">
+                <blockquote class="icon larger">
+                  <p>"Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus ac facilisis in, egestas eget."</p>
+                  <div class="blockquote-details justify-content-center">
+                    <div class="img-blob blob1"><img src="style/images/art/t3.jpg" alt="" /></div>
+                    <div class="info">
+                      <h6 class="mb-0">Barclay Widerski</h6>
+                      <span class="meta mb-0">Sales Manager</span>
+                    </div>
+                  </div>
+                </blockquote>
+              </div>
+              <!-- /.item -->
+            </div>
+            <!-- /.owl-carousel -->
+          </div>
+          <!-- /column -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
+    </div>
+    <!-- /.wrapper -->
+    
+    <?php include 'footer.php'; ?>
+
+    
+  </div>
+  <!-- /.content-wrapper -->
+  <script src="style/js/jquery.min.js"></script>
+  <script src="style/js/popper.min.js"></script>
+  <script src="style/js/bootstrap.min.js"></script>
+  <script src="style/js/plugins.js"></script>
+  <script src="style/js/scripts.js"></script>
+</body>
+</html>
